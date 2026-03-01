b1706a3695197b060902d428a3d84f2d46bf2a38
Carlos Leal <carlosleal2342@gmail.com>
2026-03-01 00:32:24 -0400

    Initial commit

---
+++ bases/app.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const msg = 'Hola Mundo';
+const hero = {
+    name: 'Ironman',
+    age: 45,
+};
+// hero.age = '50'; //Arroja un error de tipado
+hero.age = 50;
+console.log(hero.age);
+//# sourceMappingURL=app.js.map
---
+++ bases/app.ts
@@ -0,0 +1,11 @@
+const msg: string = 'Hola Mundo';
+
+const hero = {
+  name: 'Ironman',
+  age: 45,
+};
+
+// hero.age = '50'; //Arroja un error de tipado
+hero.age = 50;
+
+console.log(hero.age);
