d639bca9226649e707b6f529faf9ece8f02278cf
Carlos Leal <carlosleal2342@gmail.com>
2026-03-12 19:03:00 -0400

    Update express-api

---
+++ express-api/dist/index.js
@@ -0,0 +1,19 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+// const express = require('express');
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+const port = 3000;
+app.get('/', (req, res) => {
+    res.status(401).json({
+        ok: false,
+        msg: 'No hay token en la petición',
+    });
+});
+app.listen(port, () => {
+    console.log(`Escuchando en el puerto ${port}`);
+});
+//# sourceMappingURL=index.js.map
--- express-api/index.ts
+++ express-api/index.ts
@@ -1,11 +1,13 @@
-const express = require('express');
+// const express = require('express');
+import express from 'express';
+
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.json({
-    ok: true,
-    msg: 'Todo salió bien!',
+  res.status(401).json({
+    ok: false,
+    msg: 'No hay token en la petición',
   });
 });
 
